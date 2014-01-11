f652c211dc0c5433901d561bf2b7ce56ab92170e
bredele <olivier.wietrich@gmail.com>
2014-01-11 15:07:06 -0700

    add to array utils and test

--- test/emitter.js
+++ test/emitter.js
@@ -14,8 +14,37 @@ describe("Emitter", function() {
 		});
 	});
 
-	describe(".on", function() {
-		
+	describe(".on(event, fn)", function() {
+
+		it('should register a listener', function() {
+			var emitter = new Emitter(),
+					call = "";
+
+			emitter.on('foo', function(val) {
+				call = val;
+			});
+			emitter.emit('foo', 'maple');
+
+			assert.equal(call, 'maple');
+		});
+
+		it('should execute listeners in the order', function() {
+			var emitter = new Emitter(),
+					calls = [];
+
+			emitter.on('foo', function(val) {
+				calls.push('one', val);
+			});
+			emitter.on('foo', function(val) {
+				calls.push('two', val);
+			});
+
+			emitter.emit('foo', 0);
+			emitter.emit('foo', 1);				
+
+			assert.deepEqual(calls, ['one', 0, 'two', 0, 'one', 1, 'two', 1]);
+		});
+
 	});
 	
 	
--- test/utils.js
+++ test/utils.js
@@ -1,8 +1,9 @@
 var assert = require('assert'),
-		mixin = require('maple/lib/utils').mixin;
+		utils = require('maple/lib/utils');
 
 describe("Utils", function() {
 	describe("mixin", function() {
+		var mixin = utils.mixin;
 		it("merge two objects", function() {
 			var from = {label: 'utils'},
 			    to = {};
@@ -11,5 +12,20 @@ describe("Utils", function() {
 			assert.equal(to.label, 'utils');
 		});
 		
-	});	
+	});
+
+	describe("to array", function() {
+		var toArray = utils.toArray;
+		it("transform an array-like into an array", function() {
+			var arr = null;
+			var fn = function() {
+				arr = toArray(arguments);
+			};
+			fn('maple', 'canada');
+			assert(arr instanceof Array);
+			assert.deepEqual(arr, ['maple', 'canada']);
+		});
+		
+	});
+	
 });
