b161c3e4d50c2060d68c0d6cb0122c563c614b36
bredele <olivier.wietrich@gmail.com>
2014-02-07 15:06:27 -0700

    add api tests

--- test/newview.js
+++ test/newview.js
@@ -1,2 +1,31 @@
 var assert = require('assert'),
     View = require('maple/newview');
+
+describe('View', function() {
+
+	describe('API', function() {
+
+		it('should have a html method', function() {
+			var view = new View();
+			assert.equal(typeof view.html, 'function');
+		});
+
+		it('should have a el method', function() {
+			var view = new View();
+			assert.equal(typeof view.el, 'function');
+		});
+
+		it('should have a plug method', function() {
+			var view = new View();
+			assert.equal(typeof view.plug, 'function');			
+		});
+
+		it('should have a remove method', function() {
+			var view = new View();
+			assert.equal(typeof view.remove, 'function');
+		});
+		
+	});
+	
+});
+        
