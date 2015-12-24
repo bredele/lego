1c827b9803e819be969ddd66c3c455080dd56bf6
bredele <olivier.wietrich@gmail.com>
2015-12-23 23:15:15 -0700

    refactor data binding tests

--- index.js
+++ index.js
@@ -3,12 +3,10 @@
  * Module dependencies.
  */
 
-var fragment = require('fragment');
-var Store = require('datastore');
-var walk = require('domwalk');
-var mouth = require('mouth');
 var dom = require('stomach');
-var many = require('many');
+var mouth = require('mouth');
+var walk = require('domwalk');
+var Store = require('datastore');
 
 
 /**
@@ -21,7 +19,7 @@ module.exports = function(tmpl, data, anchor) {
     data = {};
   }
   var brick = new Brick(tmpl, data);
-  if(anchor) brick.build().to(anchor);
+  if(anchor) brick.to(anchor);
   return brick;
 };
 
@@ -33,15 +31,14 @@ module.exports = function(tmpl, data, anchor) {
  *
  *   var lego = brick('<button>');
  *   var lego = brick('<button>', data);
- * 
+ *
  * @param {String | Element?} tmpl
  * @param {Object?} data
  * @api public
  */
 
 function Brick(tmpl, data) {
-  Store.call(this, data);
-  this.state = 'created';
+  Store.call(this, data || {});
   this.from(tmpl);
 }
 
@@ -49,116 +46,35 @@ function Brick(tmpl, data) {
 Brick.prototype = Store.prototype;
 
 
-/**
- * Add state machine transition
- * aka hook.
- *
- * Listen to a change of state or
- * define a state transition callback.
- *
- * Examples:
- *
- *   // transition on event lock
- *   lego.hook('created', 'lock', 'locked');
- *   lego.emit('lock');
- *   
- *   // with callback
- *   lego.hook('created', 'lock', function() {
- *     // do something
- *   }, 'locked');
- * 
- * @param  {String}   before
- * @param  {String}   ev
- * @param  {Function?} cb
- * @param  {String?}   after
- * @return {this}
- * @api public
- */
+Brick.prototype.from = function(tmpl) {
+  this.el = dom(tmpl);
+  return this;
+};
 
-Brick.prototype.hook = function(before, ev, cb, after) {
-  if(typeof cb === 'string') {
-    after = cb;
-    cb = null;
-  }
-  var that = this;
-  this.on(ev, function() {
-    if(that.state === before) {
-      cb && cb.apply(that, arguments);
-      if(after) that.state = after;
-    }
-  });
+Brick.prototype.to = function(anchor) {
+  anchor.appendChild(this.el);
   return this;
 };
 
 
 /**
- * Create brick dom element from
- * string or existing dom element.
- * 
- * @param  {String | Element}  tmpl
- * @param {Boolean?} bool clone node
- * @return {this}
- * @api public
- */
-
-Brick.prototype.from = function(tmpl, bool) {
-  this.el = (typeof tmpl === 'function')
-    ? tmpl(this)
-    : dom(tmpl, bool);
-  return this;
-};
-
-
-/**
- * Add attribute binding.
+ * Bind DOM node with data using
+ * mouth template engine.
  *
- * As seen below, a brick can bind
- * existing attributes, dataset or
- * custom attributes.
- *
- * Examples:
- *
- *   lego.attr('class', fn);
- *   lego.attr('awesome', fn);
- *   lego.attr('data-test', fn);
- *   lego.attr({
- *     class: fn,
- *     'data-test': cb
- *   });
- * 
- * @param  {String | Object} name 
- * @param  {Function?} binding
- * @return {this}
- * @api public
- */
-
-Brick.prototype.attr = many(function(name, binding) {
-  if(this.el.hasAttribute(name)) binding.call(this, this.el, this.el.getAttribute(name));
-  this.query('[' + name + ']', function(node) {
-    binding.call(this, node, node.getAttribute(name));
-  });
-  return this;
-});
-
-
-/**
- * Query all nodes inside a brick.
- *
- * Examples:
- *
- *  lego.query('input', function() {
- *    // do something
- *  })
- * 
- * @param {String} selector
- * @param {Function} cb
+ * @return {Element} node
  * @api private
  */
 
-Brick.prototype.query = function(selector, cb) {
-  var els = this.el.querySelectorAll(selector);
-  for(var i = 0, l = els.length; i < l; i++) {
-    cb.call(this, els[i]);
+Brick.prototype.bind = function(node) {
+  var data = this.data;
+  var compiled = mouth(node.nodeValue, data);
+  var keys = compiled[1];
+  var fn = function() {
+    node.nodeValue = compiled[0](data);
+  };
+  fn();
+  for(var l = keys.length; l--;) {
+    this.on('change ' + keys[l], fn);
   }
   return this;
 };
@@ -187,103 +103,3 @@ Brick.prototype.build = function() {
   return this;
 };
 
-
-/**
- * Bind DOM node with data using
- * mouth template engine.
- *
- * @return {Element} node
- * @api private
- */
-
-Brick.prototype.bind = function(node) {
-  var data = this.data;
-  var content = node.nodeValue;
-  var compiled = mouth(content, data);
-  var keys = compiled[1];
-  var fn = function() {
-    node.nodeValue = compiled[0](data);
-  };
-  fn();
-  for(var l = keys.length; l--;) {
-    this.on('change ' + keys[l], fn);
-  }
-  return this;
-};
-
-
-/**
- * Add custom element.
- *
- * Brick allows you to create your
- * own tags (with the web component
- * standard) or to override existing
- * one.
- *
- * Examples:
- *
- *   var list = brick('<div><user /></div>');
- *   var user = brick('<button></button>');
- *
- *   list.tag('user', user);
- * 
- * @param  {String} name
- * @param  {Brick} brick
- * @return {this}
- */
-
-Brick.prototype.tag = many(function(name, brick) {
-  brick.build();
-  this.query(name, function(node) {
-    replace(node, brick.el);
-    brick.query('content', function(content) {
-      var select = content.getAttribute('select');
-      if(select) {
-        replace(content, node.querySelector(select));
-      } else {
-        replace(content, fragment([].slice.call(node.childNodes)));
-      }
-    });
-  });
-  return this;
-});
-
-
-/**
- * Append brick to
- * dom element.
- *
- * Examples:
- *
- *   // dom element
- *   var foo = brick(tmpl);
- *   foo.to(document.body);
- *
- *   // query selector
- *   var bar = brick(tmpl);
- *   bar.to('.article');
- * 
- * @param  {Element | String} el
- * @return {this}
- * @api public
- */
-
-Brick.prototype.to = function(el) {
-  dom(el).appendChild(this.el);
-  return this;
-};
-
-
-/**
- * Replace one node with another.
- *
- * @note benchmark vs remove/insertBefore
- * 
- * @param {Element} old
- * @param {Element} el
- * @api private
- */
-
-function replace(old, el) {
-  old.parentNode.replaceChild(el, old);
-}
--- test/brick.js
+++ test/brick.js
@@ -7,399 +7,110 @@ var assert = require('assert');
 var brick = require('..');
 
 
-describe("API", function() {
-
-  var obj;
-  beforeEach(function() {
-    obj = brick();
-  });
-  
-
-  it("should be a component/emitter", function() {
-    assert(obj.emit);
-    assert(obj.on);
-    assert(obj.once);
-    assert(obj.off);
-  });
-  
-  it('should be a bredele/datastore', function() {
-    assert(obj.set);
-    assert(obj.get);
-    assert(obj.del);
-    assert(obj.reset);
-    assert(obj.compute);
-  });
-
-  it("should have the following API", function() {
-    assert(obj.from);
-    //assert(obj.mold);
-    assert(obj.tag);
-    assert(obj.build);
-    assert(obj.attr);
-    assert(obj.hook);
-    assert(obj.use);
-  });
-  
-});
-
-describe("#hook", function() {
-
-  var obj;
-  beforeEach(function() {
-    obj = brick();
-  });
-
-  it('should have an initial state', function() {
-    assert.equal(obj.state, 'created');
-  });
-
-  it("should add transition", function(done) {
-    obj.hook('created', 'lock', function() {
-      done();
-    }, 'locked');
-    obj.emit('lock');
-  });
-
-  it("should set current state", function() {
-    obj.hook('created', 'lock', function(){}, 'locked');
-    obj.emit('lock');
-    assert.equal(obj.state, 'locked');
-  });
-
-  it('should not change current state', function() {
-    obj.hook('created', 'lock', function(){});
-    obj.emit('lock');
-    assert.equal(obj.state, 'created');
-  });
-
-  it('should always change state', function() {
-    obj.hook('created', 'lock', null, 'locked');
-    obj.emit('lock');
-    assert.equal(obj.state, 'locked');
-  });
-
-  it('should perform transition without callback', function() {
-    obj.hook('created', 'lock', 'locked');
-    obj.emit('lock');
-    assert.equal(obj.state, 'locked');
-  });
-
-  it('should pass arguments', function(done) {
-    obj.hook('created', 'lock', function(hello, world){
-      if(hello === 'hello') done();
-    });
-    obj.emit('lock', 'hello');
-  });
-
-});
-
-
-describe("#from", function() {
-
-  var obj;
-  beforeEach(function() {
-    obj = brick();
-  });
-
-  it("should render string into dom", function() {
-    obj.from('<button>hello</button>');
-    assert.equal(obj.el.innerHTML, 'hello');
-    assert.equal(obj.el.nodeName, 'BUTTON');
-  });
-
-  it("should render from existing dom", function() {
-    var div = document.createElement('ul');
-    obj.from(div);
-    
-    assert.equal(obj.el.nodeName, 'UL');
-  });
-
-  it('should render from query selection', function() {
-    document.body.insertAdjacentHTML('beforeend', '<section class="brick-test">');
-    obj.from('.brick-test');
-
-    assert.equal(obj.el.nodeName, 'SECTION');
-    assert.equal(obj.el.getAttribute('class'), 'brick-test');
-  });
-
-  it('should render from other template engine', function(done) {
-    obj.from(function(data) {
-      if(data === obj) done();
-    });
-  });
-
-});
-
-describe("#attr", function() {
-  
-  var obj;
-  beforeEach(function() {
-    obj = brick();
-    obj.from('<section class="section" data-test="hello">content</section>');
-  });
-
-  it("should apply binding", function(done) {
-    obj.attr('data-test', function(node, content) {
-      if(content === 'hello') done();
-    });
-    obj.build();
-  });
-
-  it('should scope binding with itself', function(done) {
-    obj.set('name', 'olivier');
-    obj.attr('data-test', function(node, content) {
-      if(this.get('name') === 'olivier') done();
-    });
-    obj.build();
-  });
-
-  it('should apply multiple bindings', function() {
-    var result = '';
-    obj.attr({
-      'data-test' : function(node, content) {
-        result += content;
-      },
-      'class' : function(node, content) {
-        result += content;
-      }
-    });
-    obj.build();
-    assert.equal(result, 'hellosection');
-  });
-
-});
-
-
-describe("Constructor", function() {
-
-  it("should set template", function() {
-    var obj = brick('<button>hello</button>');
-    assert.equal(obj.el.innerHTML, 'hello');
-    assert.equal(obj.el.nodeName, 'BUTTON');
-  });
-  
-  it("should set model", function() {
-    var obj = brick('<button>hello</button>', {
-      name: 'olivier'
-    });
-    assert.equal(obj.get('name'), 'olivier');
-
-  });
-});
-
-// describe("#mold", function() {
-
-//   it("should substitute single expression", function() {
-//     var obj = brick('<button>${label}</button>', {
-//       label: 'olivier'
-//     });
-//     obj.build();
-
-//     assert.equal(obj.el.innerHTML, 'olivier');
-//   });
-
-//   it("should substitue multiple expressions in the same node", function() {
-//     var obj = brick('<button>${label} from ${country}</button>', {
-//       label: 'olivier',
-//       country: 'france'
-//     });
-//     obj.build();
-
-//     assert.equal(obj.el.innerHTML, 'olivier from france');
-//   });
-  
-//   it("should substitue every text node", function() {
-//     var obj = brick('<button class="${country}">${label}</button>', {
-//       label: 'olivier',
-//       country: 'france'
-//     });
-//     obj.build();
-
-//     assert.equal(obj.el.className, 'france');
-//     assert.equal(obj.el.innerHTML, 'olivier');
-//   });
-
-//   describe('live interpolation', function() {
-
-//     it("should update text node on model change", function() {
-//       var obj = brick('<button>${label}</button>', {
-//         label: 'olivier'
-//       });
-//       obj.build();
-//       obj.set('label', 'bredele');
-//       assert.equal(obj.el.innerHTML, 'bredele');
-//     });
-    
-//   });
-  
-// });
-
-// describe("#mold", function() {
-  
-//   it("should return a new brick", function() {
-//     var obj = brick('<section class="section">')
-//       .attr('class', function(node, content) {
-//         node.innerHTML = content;
-//       }).mold();
-
-
-//     var other = obj().build();
-//     assert.equal(other.el.innerHTML ,'section');
-//   });
-  
-// });
-
-describe('#tag', function() {
-
-  it("should replace custom tag with brick", function() {
-    var list = brick('<ul><user></user></ul>');
-    var user = brick('<h1>user</h1>');
-
-    list.tag('user', user);
-    list.build();
-
-    assert.equal(list.el.innerHTML, '<h1>user</h1>');
-  });
-
-  // note: une brick peut elle etre un custom element?
-  // dans ce cas on doit verifier le this.el (c pk il faudrait
-  // peut etre mettre la brick dans unf ragment par defaut)
-  //var list = brick('<user><span>hello world!</span></user>');
-
-  it('should replace the content of a custom element with 1 node', function() {
-    var list = brick('<div><user>  <button>hello</button></user></div>');
-    var user = brick('<div><content></content></div>');
-
-    list.tag('user', user);
-    list.build();
-
-    // note on doit utiliser un fragment 
-    // ce sera plus rapide
-    assert.equal(user.el.innerHTML, '  <button>hello</button>');
-  });
-
-
-  it('should replace the content of a custom element with multiple nodes', function() {
-    var list = brick('<div><user>  <h1>hello</h1><button>world</button></user></div>');
-    var user = brick('<div><h2>brick</h2><content></content></div>');
-
-    list.tag('user', user);
-    list.build();
-
-    // note on doit utiliser un fragment 
-    // ce sera plus rapide
-    assert.equal(user.el.innerHTML, '<h2>brick</h2>  <h1>hello</h1><button>world</button>');
-  });
-
-  it('should replace the content of a custom element with query selection', function() {
-    var list = brick('<div><user><h1>hello</h1><button>world</button></user></div>');
-    var user = brick('<div><content select="button"></content></div>');
-
-    list.tag('user', user);
-    list.build();
-
-    // note on doit utiliser un fragment 
-    // ce sera plus rapide
-    assert.equal(user.el.innerHTML, '<button>world</button>');
-  });
-
-  it('should bind a custom element inner content', function() {
-    var list = brick('<div><user>${name}</user></div>', {
-      name: 'olivier'
-    });
-    var user = brick('<div>${name} and <content></content></div>', {
-      name: 'bruno'
-    });
-    list.tag('user', user);
-    list.build();
-
-    assert.equal(user.el.innerHTML, 'bruno and olivier');
-
-    list.set('name', 'bredele');
-
-    assert.equal(user.el.innerHTML, 'bruno and bredele');
-
-    user.set('name', 'amy');
-    assert.equal(user.el.innerHTML, 'amy and bredele');
-
-  });
-
-});
-
-describe("#to", function() {
-
-  var doc, obj;
-  beforeEach(function() {
-    doc = document.createElement('div');
-    obj = brick('<button class="${name}">', {
-      name: 'olivier'
-    });
-  });
-
-  it("should append a brick into a dom element", function() {
-    obj.to(doc);
-    assert.equal(doc.firstChild, obj.el);
-  });
-
-  // it("should also build a brick", function() {
-  //   obj.to(doc);
-  //   assert.equal(obj.el.className, 'olivier');
-  // });
-  
-  it("should query select a dom element and append the brick to it", function() {
-    document.body.insertAdjacentHTML('beforeend', '<article class="article">');
-    obj.to('.article');
-    assert.equal(document.querySelector('.article').firstChild, obj.el);
-  });
-
-});
-
 
 describe('constructor', function() {
-  var anchor;
-  beforeEach(function() {
-    anchor = document.createElement('div');
-  });
-  
-  it('should build brick if data and anchor are specified', function() {
-    brick('<button>${name}</button>', {
-      name: 'olivier'
-    }, anchor);
-    assert.equal(anchor.firstChild.outerHTML, '<button>olivier</button>');
+
+  describe('template', function() {
+
+    it('should accept html element', function() {
+      var lego = brick(document.body);
+      assert.equal(lego.el, document.body);
+    });
+
+    it('should accept html string', function() {
+      var lego = brick('<button>hello world</button>');
+      assert.equal(lego.el.outerHTML, '<button>hello world</button>');
+    });
+
+    it('should accept html query selector', function() {
+      var lego = brick('body');
+      assert.equal(lego.el, document.body);
+    });
+
   });
 
-  it('should build brick if anchor is specified', function() {
-    brick('<button>olivier</button>', anchor);
-    assert.equal(anchor.firstChild.outerHTML, '<button>olivier</button>');
+  describe('datastore', function() {
+
+    it('should pass optional data', function() {
+      var lego = brick(document.body, {
+        name: 'olivier'
+      });
+      assert.equal(lego.get('name'), 'olivier');
+    });
+
   });
+
+  describe('insert', function() {
+
+    it('should insert brick element without data', function() {
+      var div = document.createElement('div');
+      brick('<button>hello world</button>', div);
+      assert.equal(div.innerHTML, '<button>hello world</button>');
+    });
+
+    it('should insert brick element with data', function() {
+      var div = document.createElement('div');
+      brick('<button>hello world</button>', {}, div);
+      assert.equal(div.innerHTML, '<button>hello world</button>');
+    });
+
+  });
+
 });
 
-// var anchor = document.createElement('div');
-// for(var l = 1000; l--;) {
-//   anchor.insertAdjacentHTML('beforeend', '<span>${ label }</span>');
-// }
-// var t0 = performance.now();
-// btn = brick(anchor, {
-//     label: 'amy'
-// });
-// btn.to(document.body);
-// console.log(performance.now() - t0);
 
-// var t0 = performance.now();
-// for(var l = 10000; l--;) {
-//   var btn = brick('<button>${label}</button>', {
-//     label: 'klara'
-//   });
-//   btn.build();
-//   btn.to(document.body);
-// }
-// console.log(performance.now() - t0);
+describe('data binding', function() {
 
-// var btn = brick('<button>${label}</button>');
+  describe('node type 2', function() {
 
-// btn.to(document.body);
+    var lego;
+    beforeEach(function() {
+      lego = brick(document.body, {
+        label: 'hello world'
+      });
+    });
 
-// var i = 0;
-// setInterval(function() {
-//   btn.set('label', i++);
-// }, 0);
+    it('should bind one dom node with data', function() {
+      var node = document.createTextNode('${label}');
+      lego.bind(node);
+      assert.equal(node.nodeValue, 'hello world');
+    });
+
+    it('should update node when data changes', function() {
+      var node = document.createTextNode('${label}');
+      lego.bind(node);
+      lego.set('label', 'olivier');
+      assert.equal(node.nodeValue, 'olivier');
+    });
+
+    it('should update node only once when data changes', function() {
+      var node = document.createTextNode('#{label}');
+      lego.bind(node);
+      lego.set('label', 'olivier');
+      assert.equal(node.nodeValue, 'hello world');
+    });
+
+  });
+
+  describe('node type 1', function() {
+
+    it('should bind node and all its children with data', function() {
+      var lego = brick('<button class="btn ${label}">hello ${label}</button>', {
+        label: 'world'
+      }).build();
+
+      assert.equal(lego.el.outerHTML, '<button class="btn world">hello world</button>');
+    });
+
+    it('should mix one time binding and real time binding', function() {
+      var lego = brick('<button class="btn #{label}">hello ${label}</button>', {
+        label: 'world'
+      }).build();
+      lego.set('label', 'olivier');
+
+      assert.equal(lego.el.outerHTML, '<button class="btn world">hello olivier</button>');
+    });
+
+  })
+
+});
