767b3826d68f5b6cc8c00e16953ee670c4af9a9c
bredele <olivier.wietrich@gmail.com>
2016-09-05 15:16:29 -0600

    create brick element from existing DOM element

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
 
 
+/**
+ * Expose 'brick'
+ */
+
 module.exports = function(tmpl) {
   return new Brick(tmpl)
 }
 
 
+/**
+ * Brick constructor.
+ *
+ * Examples:
+ *
+ *   var lego = brick('<button>');
+ *   var lego = brick('<button>', data);
+ *
+ * @param {String | Element?} tmpl
+ * @param {Object?} data
+ * @api public
+ */
+
 function Brick(tmpl) {
-  var div = document.createElement('div')
-  div.innerHTML = tmpl
-  this.el = div.children[0]
+  this.el = element(tmpl)
+}
+
+
+
+
+function element(tmpl) {
+  if(typeof tmpl === 'string') {
+    var div = document.createElement('div')
+    div.innerHTML = tmpl
+    tmpl = div.children[0]
+  }
+  return tmpl
 }
--- test/create.js
+++ test/create.js
@@ -7,8 +7,16 @@ var brick = require('..')
 var tape = require('tape')
 
 
-tape('should create element', test => {
+tape('should create element from string', test => {
 	test.plan(1)
-	var btn = brick('<button>hello</button>')
-	test.equal(btn.el.outerHTML, '<button>hello</button>')
+	var btn = brick('<button></button>')
+	test.equal(btn.el.outerHTML, '<button></button>')
+})
+
+
+tape('should wrap existing DOM element', test => {
+	test.plan(1)
+	var el = document.createElement('button')
+	var btn = brick(el)
+	test.equal(btn.el.outerHTML, '<button></button>')
 })
