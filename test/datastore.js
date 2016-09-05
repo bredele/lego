01929f991ece8ac16a1e1f84e8bd4e42a3a8387f
bredele <olivier.wietrich@gmail.com>
2016-09-05 16:12:55 -0600

    expose datastore api

--- test/datastore.js
+++ test/datastore.js
@@ -7,6 +7,18 @@ var brick = require('..')
 var tape = require('tape')
 
 
+tape('should be a datastore', test => {
+	test.plan(7)
+	var ui = brick()
+	test.equal(typeof ui.set, 'function')
+	test.equal(typeof ui.get, 'function')
+	test.equal(typeof ui.del, 'function')
+	test.equal(typeof ui.reset, 'function')
+	test.equal(typeof ui.on, 'function')
+	test.equal(typeof ui.emit, 'function')
+	test.equal(typeof ui.off, 'function')
+})
+
 tape('should create brick element with data', test => {
 	test.plan(1)
 	var btn = brick('<button>', {
