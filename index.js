521a6ca1abb0b518fc74aa383a8ad3ab35d64a67
bredele <olivier.wietrich@gmail.com>
2016-10-29 19:08:15 -0600

    create a DOM element

--- index.js
+++ index.js
@@ -2,14 +2,23 @@
  * Dependencies
  */
 
-var store = require('datastore').factory
+var store = require('datastore')
 
 
 /**
  *
  */
- 
-module.exports = function() {
-  var brick = store({})
+
+module.exports = function(tmpl, target) {
+  target = target || {}
+  var brick = store(target.data)
+  brick.el = domify(tmpl)
   return brick
+}
+
+
+function domify(tmpl) {
+  var div = document.createElement('div')
+  div.innerHTML = tmpl
+  return div.children[0]
 }
--- test/legoh.js
+++ test/legoh.js
@@ -12,3 +12,20 @@ test('should be a datastore', assert => {
   brick.set('name', 'olivier')
   assert.equal(brick.get('name'), 'olivier')
 })
+
+
+test('should initialize datastore with data', assert => {
+  assert.plan(1)
+  var brick = lego('', {
+    data : {
+      name : 'olivier'
+    }
+  })
+  assert.equal(brick.get('name'), 'olivier')
+})
+
+test('should create a DOM element from a string', assert => {
+  assert.plan(1)
+  var btn = lego('<button>hello</button>')
+  assert.equal(btn.el.outerHTML, '<button>hello</button>')
+})
