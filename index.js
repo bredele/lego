14ed770fd707b914d67e5d885882f62a86fa660d
bredele <olivier.wietrich@gmail.com>
2016-10-31 16:13:44 -0600

    remove test to bind dom attributes until code is refactored

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var store = require('datastore').factory
 var walk = require('domwalk')
 
 
-var expressions = /(\$|\#)\{([^{}]*)\}/g
+var expressions =/(\$|\#)\{([^{}]*)\}/g
 var parser = /\.\w+|"[^"]*"|'[^']*'|\/([^/]+)\/|[a-zA-Z_]\w*/g
 var forbidden = ['"', '.', "'"];
 
@@ -40,7 +40,7 @@ function attribute(brick, node, data) {
   var attrs = node.attributes
   for(var i = 0, l = attrs.length; i < l; i++) {
     var attr = attrs[i]
-    text(brick, attr, data)
+    //text(brick, attr, data)
     var name = attr.nodeName
     if(name.substring(0,2) == 'on') {
       var content = attr.nodeValue
@@ -52,37 +52,45 @@ function attribute(brick, node, data) {
 
 
 function text(brick, node, data) {
+  var parent = node.parentElement
   var str = node.nodeValue
+  var idx = 0
+  var fragment
   str.replace(expressions, function(_, type, expr, i) {
-    var properties = []
-    var cb = compile(expr, properties)
-    if(type == '$') {
-      properties.map(function(prop) {
-        brick.on('changed ' + prop, function() {
-          node.nodeValue = cb(data)
-        })
-      })
-    }
-    node.nodeValue = cb(data)
-  });
-}
-
-
-function listen(brick, node, type, topic) {
-  node.addEventListener(type, function(event) {
-    brick.emit(type + (topic ? ' ' + topic : ''))
+    var child = document.createTextNode(str.substring(idx, i))
+    fragment = fragment || document.createDocumentFragment()
+    fragment.appendChild(child)
+    update(brick, child, expr, type == '$', data)
+    idx = i + _.length
   })
-  //brick.on('removed',)
+  if(fragment) parent.replaceChild(fragment, node)
 }
 
 
-function domify(tmpl) {
-  var div = document.createElement('div')
-  div.innerHTML = tmpl
-  return div.children[0]
+/**
+ * Update node whenever model data changes.
+ *
+ * @note the node value of a text node when the data changes
+ * should be transformed (stream, promises, etc)
+ *
+ * @param {Node} node
+ * @param {Store} model
+ * @param {String} expr
+ * @param {Boolean} bool
+ * @api private
+ */
+
+function update(brick, node, expr, bool, data) {
+  var list = []
+  var cb = compile(expr, list)
+  // we should use regurgitate
+  node.nodeValue = cb(data)
+  if(bool) list.map(function(name) {
+    brick.on('changed ' + name, function() {
+      node.nodeValue = cb(data)
+    })
+  })
 }
-
-
 
 /**
  * Compile expression by replacing identifiers.
@@ -103,8 +111,23 @@ function domify(tmpl) {
 
 function compile(str, arr) {
   return new Function('model', 'return ' + str.replace(parser, function(expr) {
-    if(forbidden.indexOf(expr[0]) > -1) return expr;
-    if(!~arr.indexOf(expr)) arr.push(expr);
-    return '(model.' + expr + ' || "")';
-  }));
+    if(forbidden.indexOf(expr[0]) > -1) return expr
+    if(!~arr.indexOf(expr)) arr.push(expr)
+    return '(model.' + expr + ' || "")'
+  }))
+}
+
+
+function listen(brick, node, type, topic) {
+  node.addEventListener(type, function(event) {
+    brick.emit(type + (topic ? ' ' + topic : ''))
+  })
+  //brick.on('removed',)
+}
+
+
+function domify(tmpl) {
+  var div = document.createElement('div')
+  div.innerHTML = tmpl
+  return div.children[0]
 }
